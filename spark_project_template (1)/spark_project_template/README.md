533d49ad1c8c3923bc21bc78fc4ab8f516f618a8
Hrushitha12 <htigulla@charlotte.edu>
2025-09-16 12:11:24 -0400

    Delete spark_project_template (1)/spark_project_template directory

--- spark_project_template (1)/spark_project_template/README.md
+++
@@ -1,3 +0,0 @@
-# ITCS 6190/8190 – Cloud Computing for Data Analysis
-
-## Course Project: Data Analysis with Apache Spark
--- spark_project_template (1)/spark_project_template/run.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-set -e
-python src/ingestion.py
-python src/transformations.py
-python src/streaming.py
-python src/ml_pipeline.py
